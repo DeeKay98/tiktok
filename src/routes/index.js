aba4c8b2f829e3050cfe1d9c8119b69a0bee1963
DeeKay98 <duykhiemm98@gmail.com>
2022-11-01 22:09:29 +0700

    Logic Header #2

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
 import { HeaderOnly } from '@/components/Layout';
+import Search from '@/components/Layout/components/Search';
 import Following from '@/pages/Following';
 import Home from '@/pages/Home';
+import Profile from '@/pages/Profile';
 import Upload from '@/pages/Upload';
 
 export const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
+    { path: '/@:nickname', component: Profile },
     { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
 ];
 
 export const privateRoutes = [];
